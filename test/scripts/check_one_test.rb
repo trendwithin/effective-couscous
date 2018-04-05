49146792dbdeebaf63599188c3853686dd6c3cd4
Michael Becco <e4e6d4d5@gmail.com>
2018-04-05 10:29:18 -0700

    Test API hit to Barchart 52-Week-Low w/ VCR

---
+++ test/scripts/check_one_test.rb
@@ -0,0 +1,10 @@
+# require 'minitest/autorun'
+require 'test_helper'
+require 'check_one'
+# class CheckOneTest < ActiveSupport::TestCase
+#   def test_existence
+#     byebug
+#     check_one = CheckOne.new
+#
+#   end
+# end
