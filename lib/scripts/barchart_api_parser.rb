9f13f870f2164bbc8b9ca64bd2668632a38d6be8
Michael Becco <e4e6d4d5@gmail.com>
2018-04-09 15:19:09 -0700

    Add Script barchart_api_parser to extract data from API endpoint

--- lib/scripts/barchart_api_parser.rb
+++ lib/scripts/barchart_api_parser.rb
@@ -25,5 +25,20 @@ module Barchart
     def parse_response_body page
       JSON.parse(page.body)
     end
+
+    def parse_data_from_api_response obj
+      format_obj = {}
+      format_obj[:ticker] = obj['symbol']
+      format_obj[:name] = obj['name']
+      format_obj[:open] = obj['open']
+      format_obj[:high] = obj['high']
+      format_obj[:low] = obj['low']
+      format_obj[:close] = obj['close']
+      format_obj[:lastPrice] = obj['lastPrice']
+      format_obj[:percentChange] = obj['percentChange']
+      format_obj[:netChange] = obj['netChange']
+      format_obj[:volume] = obj['volume']
+      format_obj
+    end
   end
 end
