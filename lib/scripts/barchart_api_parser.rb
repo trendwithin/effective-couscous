10abe431032ab3ba947fd470620f2e4d544ba8df
Michael Becco <e4e6d4d5@gmail.com>
2018-04-10 19:04:02 -0700

    Alter obj format to be more expressive, add method to insert data and rescue errors

--- lib/scripts/barchart_api_parser.rb
+++ lib/scripts/barchart_api_parser.rb
@@ -26,18 +26,36 @@ module Barchart
       JSON.parse(page.body)
     end
 
+    def map_response_and_return_formatted_record response_body
+      results = { success: 0, errors: 0 }
+      response_body['results'].map do |obj|
+        insert = parse_data_from_api_response obj
+        begin
+          StockPriceHistory.create!(insert)
+          results[:success] += 1
+        rescue ActiveRecord::NotNullViolation => record_not_null
+          results[:errors] += 1
+        rescue PG::NotNullViolation => pg_not_null
+          results[:errors] += 1
+        end
+      end
+      results
+    end
+
     def parse_data_from_api_response obj
       format_obj = {}
+      format_obj[:market_close_date] = DateTime.now.strftime("%d-%m-%Y")
       format_obj[:ticker] = obj['symbol']
-      format_obj[:name] = obj['name']
+      format_obj[:company_name] = obj['name']
       format_obj[:open] = obj['open']
       format_obj[:high] = obj['high']
       format_obj[:low] = obj['low']
       format_obj[:close] = obj['close']
-      format_obj[:lastPrice] = obj['lastPrice']
-      format_obj[:percentChange] = obj['percentChange']
-      format_obj[:netChange] = obj['netChange']
+      format_obj[:last_price] = obj['lastPrice']
+      format_obj[:percent_change] = obj['percentChange']
+      format_obj[:net_change] = obj['netChange']
       format_obj[:volume] = obj['volume']
+
       format_obj
     end
   end
