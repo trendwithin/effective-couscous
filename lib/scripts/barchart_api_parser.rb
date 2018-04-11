5224902e030e77e7025ff4e325b3d5a44e78685a
Michael Becco <e4e6d4d5@gmail.com>
2018-04-11 07:59:41 -0700

    Add Historical Data Parsing

--- lib/scripts/barchart_api_parser.rb
+++ lib/scripts/barchart_api_parser.rb
@@ -42,6 +42,39 @@ module Barchart
       results
     end
 
+    def map_response_and_return_historical_record response_body
+      results = { success: 0, errors: 0 }
+      response_body['results'].map do |obj|
+        insert = parse_historical_data_from_api_response obj
+        begin
+          StockPriceHistory.create!(insert)
+          results[:success] += 1
+        rescue ActiveRecord::NotNullViolation => record_not_null
+          results[:errors] += 1
+        rescue PG::NotNullViolation => pg_not_null
+          results[:errors] += 1
+        end
+      end
+      results
+    end
+
+    def parse_historical_data_from_api_response obj
+      format_obj = {}
+      format_obj[:market_close_date] = obj['tradingDay']
+      format_obj[:ticker] = obj['symbol']
+      format_obj[:company_name] = 'DEFAULT'
+      format_obj[:open] = obj['open']
+      format_obj[:high] = obj['high']
+      format_obj[:low] = obj['low']
+      format_obj[:close] = obj['close']
+      format_obj[:last_price] = 100000
+      format_obj[:percent_change] = 1
+      format_obj[:net_change] = 100000
+      format_obj[:volume] = obj['volume']
+
+      format_obj
+    end
+
     def parse_data_from_api_response obj
       format_obj = {}
       format_obj[:market_close_date] = DateTime.now.strftime("%d-%m-%Y")
