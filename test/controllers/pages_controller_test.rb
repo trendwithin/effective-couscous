0ed2f99588939b5d6682bb608bd7fdd7087fe89a
Michael Becco <e4e6d4d5@gmail.com>
2018-04-14 11:04:13 -0700

    Fix Broken Test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get frontpage" do
-    get pages_frontpage_url
+  test "should get frontpage as root" do
+    get root_url
     assert_response :success
   end
 
