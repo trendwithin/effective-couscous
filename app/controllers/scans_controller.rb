bf49ef729b08d7a8dca48c59fa9e15612da14f5c
Michael Becco <e4e6d4d5@gmail.com>
2018-04-17 12:19:34 -0700

    Controlelr and View for Demo

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,5 +1,19 @@
 class ScansController < ApplicationController
   def demo
-    @demo_data = StockPriceHistory.fetch_closing_data
+    @display = []
+    demo_data = StockPriceHistory.fetch_closing_data
+    sliced_data = StockPriceHistoryConcern.pg_result_slice demo_data, 250
+    sliced_data.each do |elem|
+      @display << elem.first if find_year_high_value elem
+    end
+  end
+
+  def find_year_high_value data
+    first_value = data.first['volume']
+    last_value = data.last['volume']
+    last_bool = data.max_by{ |k| k['volume'] }['volume'] == data.last['volume']
+    data.pop
+    first_bool = data.max_by { |k| k['volume']}['volume'] == data.first['volume']
+    last_bool && first_bool
   end
 end
