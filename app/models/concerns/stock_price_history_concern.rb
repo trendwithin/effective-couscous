45d9f8a015386640afeeaafc8421e512d761d5e1
Michael Becco <e4e6d4d5@gmail.com>
2018-04-17 12:18:48 -0700

    Add Concert to Extract Out Generic Code From Specific Model

--- app/models/concerns/stock_price_history_concern.rb
+++ app/models/concerns/stock_price_history_concern.rb
@@ -1,6 +1,7 @@
 module StockPriceHistoryConcern
-  include ActiveSupport::Concern
-  
+  extend ActiveSupport::Concern
+  extend self
+
   def find_year_high_value data
     first_value = data.first['volume']
     last_value = data.last['volume']
@@ -8,5 +9,9 @@ module StockPriceHistoryConcern
     data.pop
     first_bool = data.max_by { |k| k['volume']}['volume'] == data.first['volume']
     last_bool && first_bool
+  end
+
+  def pg_result_slice res, chunk_size
+    res.each_slice(chunk_size).to_a
   end
 end
