99005c019dd540b8ec00de2fcb63f2a21e56df4e
Michael Becco <e4e6d4d5@gmail.com>
2018-05-07 20:51:02 -0700

    Manually Verify Expected Results from Scripts

--- lib/scripts/parse_daily_data.rb
+++ lib/scripts/parse_daily_data.rb
@@ -1,6 +1,6 @@
 require 'mechanize'
-require_relative 'barchart_api_connect'
-require_relative 'barchart_api_parser'
+require_relative 'barchart/api_connect'
+require_relative 'barchart/barchart_api_parser'
 
 # symbols = StockSymbol.limit(1).pluck(:ticker)
 # symbols = ["A"]
@@ -8,7 +8,11 @@ symbols_list = StockSymbol.limit(2000).pluck(:ticker)
 symbols_list.each_slice(100) do |symbols|
   url = "https://marketdata.websol.barchart.com/getQuote.json?apikey=" +
          ENV['barchart_api_key'] + "&symbols=#{symbols.join(',')}"
-  page_body = Barchart::BarchartApiConnect.fetch_url url
-  response_body = Barchart::BarchartApiConnect.parse_response_body page_body
-  Barchart::BarchartApiParser.map_response_and_return_formatted_record response_body
+
+
+  connection = ApiConnect.new url
+  connection.fetch_page
+  parsed_body = connection.parse_page_response_body
+  results = Barchart::BarchartApiParser.response_body_results parsed_body
+  Barchart::BarchartApiParser.insert_historical_data results
 end
